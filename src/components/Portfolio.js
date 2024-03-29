98eb9d0e0990f43d6e649309dbf75d6cf20417b7
unknown <zandrodiocampo@gmail.com>
2024-03-29 20:55:45 +0800

    edited

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+
 export default class Porfolio extends Component {
   render() {
-    let resumeData = this.props.resumeData;
     return (
       <section id="portfolio">
-      <div className="row">
-        <div className="twelve columns collapsed">
-          <h1>Check Out Some of My Works.</h1>
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-         
-             
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Check Out Some of My Works.</h1>
+            <div className="portfolio-wrapper" style={{ display: 'flex', justifyContent: 'center' }}>
+              <div className="bgrid-quarters s-bgrid-thirds cf" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a href="#modal-01">
-                      <img src="images/3in1.jpg" className="item-img"></img>
+                    <a href="https://threeinonesample.netlify.app/">
+                      <img src="images/3in1.jpg" alt='3in1' className="item-img" />
                       <div className="overlay">
                         <div className="portfolio-item-meta">
                           <h5>3 in 1</h5>
@@ -23,48 +22,39 @@ export default class Porfolio extends Component {
                     </a>
                   </div>
                 </div>
-              
-          </div>
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-         
-             
-                <div className="columns portfolio-item">
-                  <div className="item-wrap">
-                    <a href="#modal-01">
-                      <img src="images/3in1.jpg" className="item-img"></img>
-                      <div className="overlay">
-                        <div className="portfolio-item-meta">
-                          <h5>3 in 1</h5>
-                          <p>Calcu - ToDo List - Weather App</p>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                </div>
-              
-          </div>
 
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-         
-             
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a href="#modal-01">
-                      <img src="images/3in1.jpg" className="item-img"></img>
+                    <a href="https://zdiocampo.github.io/reactionTester/">
+                      <img src="images/reaction.jpg" alt='reaction' className="item-img" />
                       <div className="overlay">
                         <div className="portfolio-item-meta">
-                          <h5>3 in 1</h5>
-                          <p>Calcu - ToDo List - Weather App</p>
+                          <h5>Reaction tester</h5>
+                          <p>How fast can you click on the shapes?</p>
                         </div>
                       </div>
                     </a>
                   </div>
                 </div>
-              
+
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="https://zdiocampo.github.io/diocampo-portfolio/">
+                      <img src="images/zdiocampo.jpg" alt='portfolio' className="item-img" />
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>First Portfolio</h5>
+                          <p>My First Portfolio Using ReactJs</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-  </section>
-        );
+      </section>
+    );
   }
 }
